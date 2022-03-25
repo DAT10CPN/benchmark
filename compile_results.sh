26597c7d7ae5a376d1fe616c167b15ae7b4f9a7a
NicEastvillage <nicoesterby@gmail.com>
2022-03-25 11:40:49 +0100

    Catch solved by simplification in reduction phase

--- compile_results.sh
+++ compile_results.sh
@@ -81,9 +81,11 @@ for COL_RED_RES_FILE in $(ls $DIR | grep "\.uout$") ; do
 	# We can check this by checking if "satisfied" is a substring of the output.
 	# If "Query is satisfied" is also a substring, then the answer is TRUE.
 	ANSWER=$([[ -n "$(echo $VOUT | awk '/satisfied/')" ]] && ([[ -n "$(echo $VOUT | awk '/Query is satisfied/')" ]] && echo "TRUE" || echo "FALSE") || echo "NONE")
+	# Was it solved by simplification in reduction?
+	ANSWER=$([[ $ANSWER = "NONE" ]] && [[ -n "$(echo $ROUT | awk '/satisfied/')" ]] && ([[ -n "$(echo $ROUT | awk '/Query is satisfied/')" ]] && echo "TRUE" || echo "FALSE") || echo "NONE")
 
 	# Was query solved using query reduction?
-	QUERY_SIMPLIFICATION=$([[ -n "$(echo $VOUT | awk '/Query solved by Query Simplification/')" ]] && echo "TRUE" || echo "FALSE")
+	QUERY_SIMPLIFICATION=$(([[ -n "$(echo $VOUT | awk '/Query solved by Query Simplification/')" ]] && echo "TRUE") || ([[ -n "$(echo $ROUT | awk '/Query solved by Query Simplification/')" ]] && echo "TRUE") || echo "FALSE")
 
 	# ----- Reduction extraction -------
 
