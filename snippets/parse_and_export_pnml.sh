c5b7696414fc73e84a26b2abf090bcaae595e12d
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-03-30 13:08:56 +0200

    update my snip

--- snippets/parse_and_export_pnml.sh
+++ snippets/parse_and_export_pnml.sh
@@ -4,3 +4,5 @@ for MODEL in $(ls $MCC_FOLDER) ; do
   ./$BINARY -R 0 -x 1 $MCC_FOLDER/$MODEL/model.pnml $MCC_FOLDER/$MODEL/ReachabilityCardinality.xml  --write-col-reduced $MCC_FOLDER/$MODEL/model.pnml --nounfold -n
 	echo "Parsed and exported $MODEL pnml file"
 done
+
+zip -r $MCC_FOLDER/../MCC2021-COL-PnmlTest.zip $MCC_FOLDER
