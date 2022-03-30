ec14b3c8ec112dbeb30cb7ea618d6e326b0811c5
MathiasMehl <33687173+MathiasMehl@users.noreply.github.com>
2022-03-30 11:55:22 +0200

    fix bugs in script (#18)

--- snippets/parse_and_export_pnml.sh
+++ snippets/parse_and_export_pnml.sh
@@ -1,6 +1,6 @@
-MCC_FOLDER = $1
-BINARY = $2
+MCC_FOLDER=$1
+BINARY=$2
 for MODEL in $(ls $MCC_FOLDER) ; do
-  ./$BINARY -R 0 -x 1 $MCC_FOLDE/$MODEL/model.pnml $MCC_FOLDE/$MODEL/ReachabilityCardinality.xml  --write-col-reduced $MCC_FOLDE/$MODEL/model.pnml --nounfold -n
+  ./$BINARY -R 0 -x 1 $MCC_FOLDER/$MODEL/model.pnml $MCC_FOLDER/$MODEL/ReachabilityCardinality.xml  --write-col-reduced $MCC_FOLDER/$MODEL/model.pnml --nounfold -n
 	echo "Parsed and exported $MODEL pnml file"
 done
