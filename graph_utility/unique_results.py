f0811077c2274634deb86cbd7f9ff2bbc6596316
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-04-13 12:04:55 +0200

    adds matrix summariser

--- graph_utility/unique_results.py
+++ graph_utility/unique_results.py
@@ -1,6 +1,7 @@
 import copy
 import os
-
+import numpy as np
+import pandas as pd
 
 def find_unique_results(options):
     os.makedirs(options.graph_dir + f'\\unique-results\\')
@@ -12,15 +13,26 @@ def find_unique_results(options):
 
     print("Starting to compare individual results")
     # Individual comparisons
+    unique_answers_matrix = pd.DataFrame()
     for index1, experiment1 in enumerate(csvs):
+        unique_answers_row = dict()
         for index2, experiment2 in enumerate(csvs):
+
             if index1 == index2:
+                unique_answers_row[experiment2.iloc[0]['test name']] = np.nan
                 continue
 
             experiment1_answers = compare_two_experiments(experiment1, experiment2)
+            unique_answers_row[experiment2.iloc[0]['test name']] = len(experiment1_answers)
             experiment1_answers.to_csv(
                 f"{graph_dir}/({experiment1.iloc[0]['test name']})_answers_that_({experiment2.iloc[0]['test name']})_did_not_find.csv")
         print(f"{(index1+1) / len(csvs) * 100:.2f}%")
+        unique_answers_matrix = unique_answers_matrix.append(unique_answers_row, ignore_index=True)
+    new_rows_indices = dict()
+    for index, name in enumerate(options.test_names):
+        new_rows_indices[index] = name
+    unique_answers_matrix = unique_answers_matrix.rename(index=new_rows_indices)
+    unique_answers_matrix.to_csv(f"{graph_dir}/unique_answers_matrix.csv")
 
 
 # Find the results that experiment1 got, that experiment2 did not
