606af8d0141b95c17f3b779c417a1509e6cf52ab
MathiasMehl <33687173+MathiasMehl@users.noreply.github.com>
2022-04-13 14:24:59 +0200

    Unique answers 2 (#34)
    * adds matrix summariser
    
    * everything
    
    * gui stuff
    
    * destroy root
    
    * import sys

--- graph_utility/unique_results.py
+++ graph_utility/unique_results.py
@@ -1,6 +1,9 @@
 import copy
 import os
 
+import numpy as np
+import pandas as pd
+
 
 def find_unique_results(options):
     os.makedirs(options.graph_dir + f'\\unique-results\\')
@@ -12,15 +15,43 @@ def find_unique_results(options):
 
     print("Starting to compare individual results")
     # Individual comparisons
+    unique_answers_matrix = pd.DataFrame()
     for index1, experiment1 in enumerate(csvs):
+        unique_answers_row = dict()
         for index2, experiment2 in enumerate(csvs):
+
             if index1 == index2:
+                unique_answers_row[experiment2.iloc[0]['test name']] = np.nan
                 continue
 
             experiment1_answers = compare_two_experiments(experiment1, experiment2)
-            experiment1_answers.to_csv(
-                f"{graph_dir}/({experiment1.iloc[0]['test name']})_answers_that_({experiment2.iloc[0]['test name']})_did_not_find.csv")
-        print(f"{(index1+1) / len(csvs) * 100:.2f}%")
+            # Add cell value for the comparison between the two experiments
+            unique_answers_row[experiment2.iloc[0]['test name']] = len(experiment1_answers)
+
+            experiment1_answers.loc[f"{experiment2.iloc[0]['test name']} answers"] = ["-----" for col in
+                                                                                      experiment1_answers.columns]
+
+            experiment2_answers = compare_two_experiments(experiment2, experiment1)
+            combined = experiment1_answers.append(experiment2_answers)
+
+            # experiment1_answers.to_csv(
+            # f"{graph_dir}/({experiment1.iloc[0]['test name']})_answers_that_({experiment2.iloc[0]['test name']})_did_not_find.csv")
+
+            if index1 < index2:
+                combined.to_csv(
+                    f"{graph_dir}/({experiment1.iloc[0]['test name']})-({experiment2.iloc[0]['test name']}).csv")
+
+        print(f"{(index1 + 1) / len(csvs) * 100:.2f}%")
+
+        # Append row to matrix
+        unique_answers_matrix = unique_answers_matrix.append(unique_answers_row, ignore_index=True)
+
+    # Write matrix
+    new_rows_indices = dict()
+    for index, name in enumerate(options.test_names):
+        new_rows_indices[index] = name
+    unique_answers_matrix = unique_answers_matrix.rename(index=new_rows_indices)
+    unique_answers_matrix.to_csv(f"{graph_dir}/unique_answers_matrix.csv")
 
 
 # Find the results that experiment1 got, that experiment2 did not
