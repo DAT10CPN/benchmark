fffd90380770dc17608e885e63f885293c5266c5
NicEastvillage <nicoesterby@gmail.com>
2022-05-10 15:10:14 +0200

    Maybe faster compile

--- compile_results_ioless.sh
+++ compile_results_ioless.sh
@@ -64,7 +64,7 @@ for MODEL in $(ls $TEST_FOLDER) ; do
 		SIZE_FILE="$DIR/$MODEL.$Q.size"
 
 		# Get stdout of model, filter out transition and place-bound statistics, and replace new lines such that regex will work
-		IN=$([[ -f $RES_FILE ]] && cat "$RES_FILE" | grep -v "^<" | tr '\n' '\r' || echo "")
+		IN=$([[ -f $RES_FILE ]] && cat "$RES_FILE" | grep -v "^<|(Query before reduction:)" | tr '\n' '\r' || echo "")
 
     CRASH="NONE"
 		CRASH=$([[ -n "$(echo IN | grep -v "^Query" | awk "/signal/")" ]] && echo "SIGNAL" || echo $CRASH)
