1eea8306b2a37ce26f57c264b15b685d2cad9f94
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-05-20 13:39:50 +0200

    fix normal graphs

--- graph_utility/gui.py
+++ graph_utility/gui.py
@@ -20,9 +20,10 @@ class Gui:
         self.max_test_in_column = 11
         self.unique_results = 0
         self.petri_net_type = ""
-        self.inhib = "Normal"
-        self.search_strategy = "BFS"
+        self.inhib = ""
+        self.search_strategy = ""
         self.all_options = False
+        self.chosen_directory = ""
 
     def set_geometry(self, root, w, h):
         ws = root.winfo_screenwidth()
@@ -31,78 +32,95 @@ class Gui:
         y = (hs / 2) - (h / 2)
         root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-    def choose_directory_category(self):
+    def choose_directory_category_search_type(self):
         root = Tk()
         root.title('Experiments')
         root.configure(bg=self.BACKGROUND)
-
-        # self.set_geometry(root, 250, 250)
 
         def set_and_continue():
             self.folder = folder_var.get()
             self.category = category_var.get()
             self.inhib = inhib_var.get()
-            if (self.inhib == "Inhib"):
-                self.category = "inhib-" + self.category
+            self.search_strategy = search_var.get()
+            self.chosen_directory = self.folder + "/" + self.category + "/" + self.search_strategy + "/" + self.inhib
             root.destroy()
 
-        # Set all categories
-        category_var = StringVar(root)
-        category_var.set("Reachability")
-        Label(root, text="Categories:", bg=self.BACKGROUND,
-              fg=self.FOREGROUND).grid(row=0, column=0)
-        dirs = ["Reachability", "CTL", "LTL", "ReachFire", "LTLFire", "CTLFire", 'Reachability-DFS']
-        for index, category_name in enumerate(dirs):
-            Radiobutton(root, text=category_name, value=category_name, variable=category_var, bg=self.BACKGROUND,
-                        fg=self.FOREGROUND).grid(row=index + 1, column=0)
+        def absolutely_everything():
+            self.all_options = True
+            root.destroy()
 
         # Set all available directories to choose from
         folder_var = StringVar(root)
         script_dir = os.path.dirname(__file__)
         results_dir = os.path.join(script_dir, '..\\results\\')
         Label(root, text="Available test directories:", bg=self.BACKGROUND,
-              fg=self.FOREGROUND).grid(row=0, column=1)
+              fg=self.FOREGROUND).grid(row=0, column=0)
         for index, test_folder in enumerate(glob.glob(results_dir + "/*/", recursive=False)):
             test_folder_name = os.path.basename(os.path.normpath(test_folder))
             if index == 0:
                 folder_var.set(test_folder_name)
             Radiobutton(root, text=test_folder_name, value=test_folder_name, variable=folder_var,
                         bg=self.BACKGROUND,
+                        fg=self.FOREGROUND).grid(row=index + 1, column=0)
+
+        # Set all categories column
+        category_var = StringVar(root)
+        category_var.set("ReachabilityCardinality")
+        Label(root, text="Categories:", bg=self.BACKGROUND,
+              fg=self.FOREGROUND).grid(row=0, column=1)
+        categories = ["ReachabilityCardinality", "ReachabilityFireability", "CTLCardinality", "CTLFireability",
+                      "LTLCardinality", "LTLFireability"]
+        for index, category_name in enumerate(categories):
+            Radiobutton(root, text=category_name, value=category_name, variable=category_var, bg=self.BACKGROUND,
                         fg=self.FOREGROUND).grid(row=index + 1, column=1)
+
+        # Set Search strategy
+        search_var = StringVar(root)
+        search_var.set("HEUR")
+        Label(root, text="Search strategy:", bg=self.BACKGROUND,
+              fg=self.FOREGROUND).grid(row=0, column=2)
+        for index, search_strategy_name in enumerate(["HEUR", "DFS", "RDFS"]):
+            Radiobutton(root, text=search_strategy_name, value=search_strategy_name, variable=search_var,
+                        bg=self.BACKGROUND,
+                        fg=self.FOREGROUND).grid(row=index + 1, column=2)
 
         # Set inhib or normal
         inhib_var = StringVar(root)
         inhib_var.set("Normal")
-        Label(root, text="Inhib or normal:", bg=self.BACKGROUND,
-              fg=self.FOREGROUND).grid(row=0, column=2)
+        Label(root, text="Type:", bg=self.BACKGROUND,
+              fg=self.FOREGROUND).grid(row=0, column=3)
         for index, category_name in enumerate(["Normal", "Inhib"]):
             Radiobutton(root, text=category_name, value=category_name, variable=inhib_var, bg=self.BACKGROUND,
-                        fg=self.FOREGROUND).grid(row=index + 1, column=2)
+                        fg=self.FOREGROUND).grid(row=index + 1, column=3)
+
+        Button(root, text="Absolutely everything", command=absolutely_everything, bg=self.BACKGROUND,
+               fg=self.FOREGROUND).grid(
+            row=len(categories) + 1, column=0)
 
         Button(root, text="Choose and continue", command=set_and_continue, bg=self.BACKGROUND, fg=self.FOREGROUND).grid(
-            row=len(dirs) + 1, column=0)
+            row=len(categories) + 1, column=3)
 
         root.eval('tk::PlaceWindow . center')
         root.protocol("WM_DELETE_WINDOW", sys.exit)
         root.mainloop()
 
     def choose_tests_and_graph_type(self):
-        directory = self.folder + "/" + self.category
         root = Tk()
         root.configure(bg=self.BACKGROUND)
-        root.title(f'{directory}')
-        # self.set_geometry(root, 300, 400)
+        root.title(f'{self.chosen_directory}')
 
-        all_csv_files_in_category_in_directory = [(filename.split(self.category)[1]).replace('\\', '') for filename in
+        all_csv_files_in_category_in_chosen_directory = [(filename.split(self.chosen_directory)[1]).replace('\\', '') for filename in
                                                   [filename for filename in
-                                                   glob.glob(os.path.join(self.results_dir + directory, "*.csv"))]]
-        if len(all_csv_files_in_category_in_directory) == 0:
-            raise Exception(f"There are no results in selected directory: {directory}")
+                                                   glob.glob(
+                                                       os.path.join(self.results_dir + self.chosen_directory, "*.csv"))]]
+
+        if len(all_csv_files_in_category_in_chosen_directory) == 0:
+            raise Exception(f"There are no results in selected directory: {self.chosen_directory}")
         results = {}
-        Label(root, text=f"{directory}:", bg=self.BACKGROUND,
+        Label(root, text=f"{self.chosen_directory}:", bg=self.BACKGROUND,
               fg=self.FOREGROUND).grid(row=0, column=1)
 
-        for index, test_name in enumerate(all_csv_files_in_category_in_directory):
+        for index, test_name in enumerate(all_csv_files_in_category_in_chosen_directory):
             column = 1
             if index > self.max_test_in_column:
                 index -= (self.max_test_in_column + 1)
@@ -122,8 +140,6 @@ class Gui:
         enable_graphs = IntVar()
         enable_graphs.set(1)
         unique_results = IntVar()
-        petri_net_type = StringVar()
-        petri_net_type.set('CPN')
         Label(root, text="Settings:", bg=self.BACKGROUND,
               fg=self.FOREGROUND).grid(row=0, column=0)
         Radiobutton(root, text='No graphs', value=0, variable=enable_graphs,
@@ -135,23 +151,17 @@ class Gui:
         Radiobutton(root, text='All graphs', value=2, variable=enable_graphs,
                     bg=self.BACKGROUND,
                     fg=self.FOREGROUND).grid(row=3, column=0)
-        Radiobutton(root, text='PT', value='PT', variable=petri_net_type,
-                    bg=self.BACKGROUND,
-                    fg=self.FOREGROUND).grid(row=4, column=0)
-        Radiobutton(root, text='CPN', value='CPN', variable=petri_net_type,
-                    bg=self.BACKGROUND,
-                    fg=self.FOREGROUND).grid(row=5, column=0)
         Checkbutton(root, text='Check consistency', variable=check_consistency,
                     bg=self.BACKGROUND,
-                    fg=self.FOREGROUND).grid(row=6, column=0)
+                    fg=self.FOREGROUND).grid(row=4, column=0)
         Checkbutton(root, text='Debug', variable=debug,
                     bg=self.BACKGROUND,
-                    fg=self.FOREGROUND).grid(row=7, column=0)
+                    fg=self.FOREGROUND).grid(row=5, column=0)
         Checkbutton(root, text='Unique results', variable=unique_results,
                     bg=self.BACKGROUND,
-                    fg=self.FOREGROUND).grid(row=8, column=0)
+                    fg=self.FOREGROUND).grid(row=6, column=0)
 
-        Button(root, text="Make graphs", command=root.destroy, bg=self.BACKGROUND, fg=self.FOREGROUND).grid(row=9,
+        Button(root, text="Make graphs", command=root.destroy, bg=self.BACKGROUND, fg=self.FOREGROUND).grid(row=7,
                                                                                                             column=0)
 
         def select_all_tests():
@@ -171,14 +181,14 @@ class Gui:
             root.destroy()
 
         Button(root, text="Select all tests", command=select_all_tests, bg=self.BACKGROUND, fg=self.FOREGROUND).grid(
-            row=10,
+            row=8,
             column=0)
         Button(root, text="Deselect all tests", command=deselect_all_tests, bg=self.BACKGROUND,
                fg=self.FOREGROUND).grid(
-            row=11,
+            row=9,
             column=0)
         Button(root, text="EVERYTHING", command=EVERYTHING, bg=self.BACKGROUND, fg=self.FOREGROUND).grid(
-            row=12,
+            row=10,
             column=0)
         root.eval('tk::PlaceWindow . center')
 
@@ -192,49 +202,59 @@ class Gui:
         self.unique_results = unique_results.get()
         if (enable_graphs.get() > 0) and (len(self.results) == 0):
             raise Exception('You did not choose any tests')
-        self.petri_net_type = petri_net_type.get()
+
+        print(self.folder)
+        if "CPN" in self.folder:
+            self.petri_net_type = "CPN"
+        elif "PT" in self.folder:
+            self.petri_net_type = "PT"
+        else:
+            raise Exception('Could not figure out if we have results from a CPN or PT. Check directory name')
+
+    def create_all_options(self):
+        # Todo
+        return []
 
     def get_options(self):
-        self.choose_directory_category()
-        self.choose_tests_and_graph_type()
+        self.choose_directory_category_search_type()
+        if not self.all_options:
+            self.choose_tests_and_graph_type()
 
-        options = Options(
-            result_dir=self.results_dir + self.folder + "\\" + self.category,
-            graph_dir=os.path.join(os.path.dirname(__file__), f"..\\graphs\\{self.folder}\\{self.category}"),
-            results_to_plot=self.results,
-            category=self.category,
-            folder=self.folder,
-            test_names=[os.path.split(os.path.splitext(csv)[0])[1] for csv in self.results],
-            chosen_graphs=['answers', 'rules', 'memory-state lines', 'time lines', 'size lines'],
-            read_results=[],
-            do_consistency_check=bool(self.do_consistency_check),
-            enable_graphs=self.enable_graphs,
-            debug=bool(self.debug),
-            unique_results=bool(self.unique_results),
-            petri_net_type=self.petri_net_type,
-            all_options=False,
-            search_strategy=self.search_strategy
-        )
+            options = Options(
+                result_dir=self.results_dir + self.chosen_directory,
+                graph_dir=os.path.join(os.path.dirname(__file__), f"..\\graphs\\{self.chosen_directory}"),
+                results_to_plot=self.results,
+                category=self.category,
+                folder=self.folder,
+                test_names=[os.path.split(os.path.splitext(csv)[0])[1] for csv in self.results],
+                chosen_graphs=['answers', 'rules', 'memory-state lines', 'time lines', 'size lines'],
+                read_results=[],
+                do_consistency_check=bool(self.do_consistency_check),
+                enable_graphs=self.enable_graphs,
+                debug=bool(self.debug),
+                unique_results=bool(self.unique_results),
+                petri_net_type=self.petri_net_type,
+                all_options=False,
+                search_strategy=self.search_strategy
+            )
 
-        if self.enable_graphs == 0:
-            options.chosen_graphs = []
+            if self.enable_graphs == 0:
+                options.chosen_graphs = []
 
-        if self.debug:
-            options.chosen_graphs.append('debug')
+            if self.debug:
+                options.chosen_graphs.append('debug')
 
-        if not ((options.enable_graphs > 0) or options.do_consistency_check or options.debug or options.unique_results):
-            raise Exception(
-                'You chose to not do graphs, consistency or debug mode, you probably clicked something wrong')
+            if not ((
+                            options.enable_graphs > 0) or options.do_consistency_check or options.debug or options.unique_results):
+                raise Exception(
+                    'You chose to not do graphs, consistency or debug mode, you probably clicked something wrong')
 
-        if len(options.results_to_plot) == 0:
-            raise Exception('You must choose some results')
+            if len(options.results_to_plot) == 0:
+                raise Exception('You must choose some results')
 
-        if len(options.results_to_plot) == 1 and (options.do_consistency_check or options.unique_results):
-            raise Exception('You must choose at least two results to do consistency/unique results')
+            if len(options.results_to_plot) == 1 and (options.do_consistency_check or options.unique_results):
+                raise Exception('You must choose at least two results to do consistency/unique results')
 
-        if self.all_options:
-            print("Doing fucking everything")
-        else:
             print("---------------Options---------------")
             print("-------Chosen tests-------")
             print(f"Folder: {options.folder}")
@@ -253,4 +273,7 @@ class Gui:
             elif self.enable_graphs == 2:
                 print(f"\tDoing all graphs")
 
-        return [options]
+            return [options]
+        else:
+            print("Doing absolutely everything")
+            return create_all_options()
