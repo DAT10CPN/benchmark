cb2ef9712a8cb32a8d7143278070be2b3c76eac9
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-05-23 21:20:36 +0200

    adds more csvs to ratios

--- graph_utility/ratios.py
+++ graph_utility/ratios.py
@@ -23,6 +23,9 @@ class VerificationTimeRatio(Lines):
     def prepare_data(self):
         if not self.options.base_name in self.options.test_names:
             return
+        os.makedirs(self.graph_dir + "csvs")
+        os.makedirs(self.graph_dir + "figs")
+
         combined = utility.combined_pd(self.data_list, self.options.test_names)
         rule_columns = [col for col in combined.columns if 'rule' in col]
         combined.drop(columns=rule_columns, inplace=True)
@@ -42,20 +45,22 @@ class VerificationTimeRatio(Lines):
                                              combined[base_metric] / combined[current_metric]),
                                          np.nan), np.nan)
                 combined[f"{current_test_name}@{metric} ratio"] = temp
+                curr = combined[f"{current_test_name}@{metric} ratio"].sort_values(ascending=False)
+                curr.to_csv(self.graph_dir + f"csvs\\{current_test_name}@{metric} ratio sorted.csv")
         columns_to_drop = [col for col in combined.columns if 'ratio' not in col]
         combined.drop(columns=columns_to_drop, inplace=True)
         self.plot_ready = combined
+        self.plot_ready.to_csv(self.graph_dir + f'csvs\\all-ratios.csv')
 
     def plot(self):
         if not self.options.base_name in self.options.test_names:
             return
-        os.makedirs(self.graph_dir)
-        self.plot_ready.to_csv(self.graph_dir + f'all-ratios.csv')
+
         for metric in self.metrics_to_do_ratios:
             relevant_columns_for_metric = [col for col in self.plot_ready.columns if metric in col]
             relevant_data = self.plot_ready[relevant_columns_for_metric].reset_index()
             relevant_data.drop(columns=['query index', 'model name'], inplace=True)
-            #rename and sort
+            # rename and sort
             for col in relevant_data:
                 test_name = col.split("@")[0]
                 relevant_data[test_name] = relevant_data[col].sort_values(ignore_index=True)
@@ -72,6 +77,6 @@ class VerificationTimeRatio(Lines):
             plt.title(f'{self.options.base_name}-{metric}/ other tests')
 
             plt.savefig(
-                self.graph_dir + f'{metric}.svg',
+                self.graph_dir + f'figs\\{metric}.svg',
                 bbox_inches='tight', dpi=600, format="svg")
             plt.close()
