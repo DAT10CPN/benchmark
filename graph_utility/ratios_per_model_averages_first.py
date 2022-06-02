c776e060e5db85fe5a77a7c0ef243fddc12d5952
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-06-02 16:23:44 +0200

    ratio stuff

--- graph_utility/ratios_per_model_averages_first.py
+++ graph_utility/ratios_per_model_averages_first.py
@@ -1,21 +1,20 @@
 import copy
 import os
 import warnings
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 
+import pandas as pd
+import numpy as np
 import utility
 from lines import Lines
 
 warnings.filterwarnings("error")
 
 
-class ModelRatios(Lines):
+class ModelRatiosAverages(Lines):
     def __init__(self, options):
         super().__init__(options)
-        self.graph_dir = options.graph_dir + '\\ratios\\model-grouped\\'
-        self.name = 'model-ratios'
+        self.graph_dir = options.graph_dir + '\\ratios\\model-grouped-average\\'
+        self.name = 'model-ratios-average'
         self.plot_ready = pd.DataFrame()
         self.metrics_to_do_ratios = ['reduce time', 'verification time', 'verification memory', 'state space size',
                                      'total time']
@@ -45,7 +44,6 @@ class ModelRatios(Lines):
             for data in self.options.read_results:
 
                 # Get total time
-                # data.set_index(["model name", "query index"], inplace=True)
                 mydata = copy.deepcopy(data)
                 test_name = mydata.iloc[0]['test name']
 
@@ -53,7 +51,7 @@ class ModelRatios(Lines):
                     continue
                 if metric == 'total time':
                     mydata[f"total time"] = utility.add_total_time(mydata, self.options.petri_net_type, False)[
-                    'total time']
+                        'total time']
                 orig_and_current = utility.combined_pd([self.options.read_results[orig_index], mydata],
                                                        [self.options.base_name, test_name])
                 rule_columns = [col for col in orig_and_current.columns if 'rule' in col]
@@ -61,18 +59,33 @@ class ModelRatios(Lines):
 
                 orig_and_current = orig_and_current[orig_and_current[f'{self.options.base_name}@answer'] != 'NONE']
                 orig_and_current = orig_and_current[orig_and_current[f'{test_name}@answer'] != 'NONE']
-
                 orig_and_current.reset_index(inplace=True)
                 orig_and_current['model name'] = orig_and_current['model name'].str.split(r'-COL').str.get(0)
-                orig_and_current = orig_and_current.groupby(['model name']).sum()
-                # orig_and_current.to_csv(self.graph_dir + f"{test_name}\\grouped_and_summed.csv")
 
                 base_metric = self.options.base_name + f'@{metric}'
                 current_metric = test_name + f'@{metric}'
+                orig_and_current['ratio'] = orig_and_current[base_metric] / orig_and_current[current_metric]
 
-                temp = orig_and_current[base_metric] / orig_and_current[current_metric]
+                def get_ratio(row):
+                    if row[base_metric] == row[current_metric]:
+                        return 1
+                    elif row[current_metric] == 0 and metric == 'verification time':
+                        return (row[base_metric] + 0.1) / 0.1
+                    elif row[base_metric] < 2 and row[current_metric] < 2:
+                        return 1
+                    elif row[current_metric] == 0:
+                        return np.nan
+                    else:
+                        return row[base_metric] / row[current_metric]
+                orig_and_current['ratio'] = orig_and_current.apply( lambda row:
+                    get_ratio(row), axis=1)
+
+                temp = orig_and_current.groupby(['model name']).mean()
+
+                # orig_and_current.to_csv(self.graph_dir + f"{test_name}\\grouped_and_summed.csv")
+
                 curr = pd.DataFrame()
-                curr[metric] = temp.round(3)
+                curr[metric] = temp['ratio'].round(3)
 
                 sorted = curr[metric].sort_values(ascending=False)
                 sorted.to_csv(self.graph_dir + f"{metric}\\{test_name} ratio sorted.csv")
@@ -84,5 +97,3 @@ class ModelRatios(Lines):
                 all_metrics_all_experiments[f"{test_name}@{metric}"] = curr
             all_ratios_in_metric.to_csv(self.graph_dir + f"{metric}\\all.csv")
         all_metrics_all_experiments.to_csv(self.graph_dir + f"all.csv")
-
-
