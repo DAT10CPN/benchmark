8b7d22643db9f617c10b33149f00eb001def4f0e
MathiasMehl <mathiasmehlsoerensen@gmail.com>
2022-06-03 11:06:35 +0200

    add more sorting to indices

--- graph_utility/graph.py
+++ graph_utility/graph.py
@@ -6,8 +6,9 @@ class Graph:
         self.options = copy.deepcopy(options)
         self.data_list = copy.deepcopy(options.read_results)
         self.name = ""
-        self.custom_sort_dict = {'orig': 0, 'C': 1, 'D': 2, 'E': 3, 'F': 4, 'I': 5, 'S': 6, 'T': 7, 'Q': 8, 'ISC': 9,
-                                 'ITC': 10, 'ISDCEFQ': 11, 'ITDCEFQ': 12}
+        self.custom_sort_dict = {'base': 0, 'orig': 0, 'C': 1, 'D': 2, 'E': 3, 'F': 4, 'I': 5, 'S': 6, 'T': 7, 'U': 8, 'Q': 9,
+                                 'ISC': 10, 'IUC': 11,
+                                 'ITC': 12, 'ISDCEFQ': 13, 'ITDCEFQ': 14, 'IUDCEFQ': 15}
 
     def plot(self):
-        print(f"Making graph: {self.name}")
+        pass
