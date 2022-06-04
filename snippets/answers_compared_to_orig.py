a0b94f4b65e11c02d4aa1e3b068c75a1d1bd4eac
NicEastvillage <nicoesterby@gmail.com>
2022-06-04 15:18:12 +0200

    Add support for PT in table scripts

--- snippets/answers_compared_to_orig.py
+++ snippets/answers_compared_to_orig.py
@@ -4,8 +4,14 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 
+# experiment_dir = "CPN-4-30-4-2-ioless"
+# relevant_experiments = ["base", "C", "D", "E", "F", "I", "Q", "U", "IUC", "IUDCEFQ"]
+
+experiment_dir = "PT-4-30-4-2-ioless"
+relevant_experiments = ["base", "newC", "withS"]
+
 root = Path(__file__).parent.parent
-data_dir = root / "results" / "CPN-4-30-4-2-ioless"
+data_dir = root / "results" / experiment_dir
 categories = [
     "ReachabilityCardinality",
     "ReachabilityFireability",
@@ -26,10 +32,11 @@ for ss in search_strategies:
     for category in categories:
         df_cat = pd.DataFrame(columns=["category", "experiment", "answers"])
         orig_answers = 0
-        for csv in (data_dir / category / ss / "MCC2021-COL").glob("*.csv"):
+        suite = "MCC2021-COL" if experiment_dir[0:3] == "CPN" else "MCC2021"
+        for csv in (data_dir / category / ss / suite).glob("*.csv"):
             df_ex = pd.read_csv(csv)
             answers = len(df_ex.index) - df_ex["answer"].value_counts()["NONE"]
-            if csv.stem == "orig":
+            if csv.stem == "base":
                 orig_answers = answers
             entry = {
                 "category": category,
@@ -48,12 +55,12 @@ for ss in search_strategies:
 
     for category in categories:
         print(f"{category:<36}", end=" & ")
-        for experiment in ["orig", "C", "D", "E", "F", "I", "Q", "U", "IUC", "IUDCEFQ"]:
+        for experiment in relevant_experiments:
             print(f'{df_ss[(df_ss["category"] == category) & (df_ss["experiment"] == experiment)].iloc[0]["answers"]:>5} & ', end="")
         print("")
     for category in categories:
         print(f"{category:<36}", end=" & ")
-        for experiment in ["orig", "C", "D", "E", "F", "I", "Q", "U", "IUC", "IUDCEFQ"]:
+        for experiment in relevant_experiments:
             if experiment == "orig":
                 print(
                     f'{int(df_ss[(df_ss["category"] == category) & (df_ss["experiment"] == experiment)].iloc[0]["answers"]):>5} & ',
@@ -62,16 +69,16 @@ for ss in search_strategies:
                 print(f'{int(df_ss[(df_ss["category"] == category) & (df_ss["experiment"] == experiment)].iloc[0]["diff"]):>+5} & ', end="")
         print("")
 
-    df_ss_no_orig = df_ss[df_ss["experiment"] != "orig"]
+    df_ss_no_orig = df_ss[df_ss["experiment"] != "base"]
 
     sns.set(rc={'figure.figsize': (11, 12)})
     sns.set_theme(style="darkgrid", palette="pastel")
     g = sns.barplot(data=df_ss_no_orig, x="diff", y="experiment", hue="category")
     for container in g.containers:
         g.bar_label(container, fmt="%+g", padding=3)
-    g.set_xlabel(f"Answers relative to orig experiment ({ss})")
+    g.set_xlabel(f"Answers relative to base experiment ({ss})")
     g.set_ylabel("Experiment")
     plt.pyplot.tight_layout()
 
-    plt.pyplot.savefig(root / "graphs" / "CPN-4-30-4-2-ioless" / f"{ss}_answers.svg", dpi=600, format="svg")
+    plt.pyplot.savefig(root / "graphs" / experiment_dir / f"{ss}_answers.svg", dpi=600, format="svg")
     plt.pyplot.close()
--- snippets/applications_table.py
+++ snippets/applications_table.py
@@ -4,8 +4,16 @@ import pandas as pd
 import matplotlib as plt
 import seaborn as sns
 
+# experiment_dir = "CPN-4-30-4-2-ioless"
+# relevant_experiments = ["base", "C", "D", "E", "F", "I", "Q", "U", "IUC", "IUDCEFQ"]
+# relevant_rules = ["C", "D", "E", "F", "I", "Q", "U"]
+
+experiment_dir = "PT-4-30-4-2-ioless"
+relevant_experiments = ["base", "newC", "withS"]
+relevant_rules = ["C", "S"]
+
 root = Path(__file__).parent.parent
-data_dir = root / "results" / "CPN-4-30-4-2-ioless"
+data_dir = root / "results" / experiment_dir
 categories = [
     "ReachabilityCardinality",
     "ReachabilityFireability",
@@ -33,18 +41,20 @@ rule_name_to_id_map = {
 
 for ss in search_strategies:
     print(f" ========= {ss} ==========")
-    df_ss = pd.DataFrame(columns=["category", "experiment", "C", "D", "E", "F", "I", "Q", "U"])
+    df_ss = pd.DataFrame(columns=["category", "experiment"] + relevant_rules)
     for category in categories:
-        df_cat = pd.DataFrame(columns=["category", "experiment", "C", "D", "E", "F", "I", "Q", "U"])
-        for csv in (data_dir / category / ss / "MCC2021-COL").glob("*.csv"):
+        df_cat = pd.DataFrame(columns=["category", "experiment"] + relevant_rules)
+        suite = "MCC2021-COL" if experiment_dir[0:3] == "CPN" else "MCC2021"
+        for csv in (data_dir / category / ss / suite).glob("*.csv"):
             df_ex = pd.read_csv(csv)
             entry = {
                 "category": category,
                 "experiment": csv.stem,
             }
-            for rule in ["C", "D", "E", "F", "I", "Q", "U"]:
-                entry[rule] = df_ex[f'rule {rule_name_to_id_map[rule]}'].sum()
-                entry[rule + "%"] = (df_ex[f'rule {rule_name_to_id_map[rule]}'] > 0).sum() / float(len(df_ex.index))
+            for rule in relevant_rules:
+                id = rule_name_to_id_map[rule] if experiment_dir[0:3] == "CPN" else rule
+                entry[rule] = df_ex[f'rule {id}'].sum()
+                entry[rule + "%"] = (df_ex[f'rule {id}'] > 0).sum() / float(len(df_ex.index))
             df_cat = df_cat.append(entry, ignore_index=True)
             print(entry)
         df_ss = pd.concat([df_ss, df_cat])
@@ -52,24 +62,24 @@ for ss in search_strategies:
 
     # Absolute applications
     for category in categories:
-        for experiment in ["C", "D", "E", "F", "I", "Q", "U", "IUC", "IUDCEFQ"]:
-            for rule in experiment:
+        for experiment in relevant_experiments:
+            for rule in relevant_rules:
                 applications = df_ss[(df_ss["category"] == category) & (df_ss["experiment"] == experiment)].iloc[0][rule]
-                if len(experiment) > 1:
-                    print(f"\multicolumn{{1}}{{c|}}{{{applications:>8}}} & ", end="")
-                else:
-                    print(f"{applications:>8} & ", end="")
+                # if len(experiment) > 1:
+                #     print(f"\multicolumn{{1}}{{c|}}{{{applications:>8}}} & ", end="")
+                # else:
+                print(f"{applications:>8} & ", end="")
         print("")
 
     print("\n")
 
     # Percentage of queries with at least one applications
     for category in categories:
-        for experiment in ["C", "D", "E", "F", "I", "Q", "U", "IUC", "IUDCEFQ"]:
-            for rule in experiment:
+        for experiment in relevant_experiments:
+            for rule in relevant_rules:
                 applications = 100 * df_ss[(df_ss["category"] == category) & (df_ss["experiment"] == experiment)].iloc[0][rule + "%"]
-                if len(experiment) > 1:
-                    print(f"\multicolumn{{1}}{{c|}}{{{applications:>6.1f}\\%}} & ", end="")
-                else:
-                    print(f"{applications:>6.1f}\\% & ", end="")
+                # if len(experiment) > 1:
+                #     print(f"\multicolumn{{1}}{{c|}}{{{applications:>6.1f}\\%}} & ", end="")
+                # else:
+                print(f"{applications:>6.1f}\\% & ", end="")
         print("")
